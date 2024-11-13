7730f49e0f2bf2ca89e23a981f5bff2f667c0bad
Dylan Tin <dylant711@gmail.com>
2024-11-12 19:51:53 -0500

    finished

---
+++ server/migrations/versions/3a3584cbfa0d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3a3584cbfa0d
+Revises: 
+Create Date: 2024-11-12 19:51:30.204685
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3a3584cbfa0d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
